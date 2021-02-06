8d57448f135d1cb9691330dd5ef1cdd33611a573
root <root@ip-172-31-40-74.us-east-2.compute.internal>
2021-02-06 04:34:48 +0000

    added 1 more file for tracking. this file subtracts 2 numbers

---
+++ subtract.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<math.h>
+
+int main()
+{
+        float num1=30;
+        float num2=13;
+        float diff=0;
+        diff=num1-num2;
+        printf("the difference is:%.2f",&diff);
+        return 0;
+
+}
+
+
