4234e23c396e0f7c054f5d3f16d1453e2f7d1b23
Aishwarya <kambale.aishwarya@gmail.com>
2021-02-06 12:25:00 +0000

    Added program for multiplication

---
+++ multiple.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+def multiple(x,y):
+       total=x * y 
+       return total
+
+def main():
+    x=7
+    y=5
+    total=multiple(7,5)
+    print("Multiplication=",total)
+
+
+if __name__ == "__main__":
+    main()
